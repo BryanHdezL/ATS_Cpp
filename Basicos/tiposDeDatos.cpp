77c5df69f6e5a28fa307cd67823df7ee7af86282
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-04 08:53:16 -0500

    Programa con los tipos de datos en C++

---
+++ Basicos/tiposDeDatos.cpp
@@ -0,0 +1,17 @@
+//Tipos de datos en C++
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Declaracion
+        int entero=15;
+        float flotante=10.45;
+        double mayor=16.3456;
+        char letra='a';
+    //Acciones
+        cout<<entero<<endl;
+        cout<<flotante<<endl;
+        cout<<mayor<<endl;
+        cout<<letra<<endl;
+    return 0;
+}
