655d48c2003161b4dab87bba4e5068c4e564d301
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-04 08:51:37 -0500

    Programa de entrada y salida

---
+++ Basicos/Entrada&Salida.cpp
@@ -0,0 +1,13 @@
+// Entrada y salida de datos.
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaracion;
+    int numero;
+    // Acciones
+    cout << "Digite un numero: ";
+    cin >> numero;
+    cout << "El numero que dijito es: " << numero << endl;
+    return 0;
+}
