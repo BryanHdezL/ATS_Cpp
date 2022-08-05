9054373ab85c1e75ca6859417a8c2eb4eeb31a5a
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-05 07:48:41 -0500

    Programa con ejemplo de las estructuras While, Do-While y For

---
+++ Ciclos/while&DoWhile&For.cpp
@@ -0,0 +1,41 @@
+//Estructuras de bucles/ciclos.
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+/*
+//Sentencia WHILE
+int main(){
+    int i=1;
+    while(i<=10){
+        cout<<i<<endl;
+        i++;
+    }
+    getch();
+    return 0;
+}
+*/
+/*
+//Sentencia DO WHILE
+int main(){
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=10);
+    system("pause");
+    return 0;
+}
+*/
+/*
+//Sentencia FOR
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<i<<endl;
+    }
+    system("pause");
+    return 0;
+}
+*/
