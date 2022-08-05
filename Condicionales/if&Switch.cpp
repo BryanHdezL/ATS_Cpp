637a5d1571dff63f499d55a7cb16995f18d9e961
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-04 21:51:14 -0500

    Programa con ejemplo de las estructuras If y Switch

---
+++ Condicionales/if&Switch.cpp
@@ -0,0 +1,34 @@
+//Estructuras condicionales.
+
+#include <iostream>
+using namespace std;
+
+//Sentencia IF
+/*
+int main(){
+    int numero,dato=5;
+    cout<<"Digite un numero: "; cin>>numero;
+    if(numero>dato){
+        cout<<"El numero es mayor a 5"<<endl;
+    }else{
+        cout<<"El numero es menor a 5"<<endl;
+    }
+    return 0;
+}
+*/
+/*
+//Sentencia SWITCH
+int main(){
+    int numero;
+    cout<<"Digite un numero entre 1-5: "; cin>>numero;
+    switch(numero){
+        case 1: cout<<"Es el numero 1"; break;
+        case 2: cout<<"Es el numero 2"; break;
+        case 3: cout<<"Es el numero 3"; break;
+        case 4: cout<<"Es el numero 4"; break;
+        case 5: cout<<"Es el numero 5"; break;
+        default: cout<<"No esta en el rango de 1-5"; break;
+    }
+    return 0;
+}
+*/
