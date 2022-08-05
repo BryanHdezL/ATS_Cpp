912d0b2602ede0ab1a7890f0588133391bb277ed
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-04 21:58:54 -0500

    Ejercicio #3 determinal numeros pares e impares

---
+++ Condicionales/ej3_condicional.cpp
@@ -0,0 +1,27 @@
+//Ejercicio #3 determina los numeros pares e impares.
+
+/***********************************************************************
+Realice un programa que lea un valor entero y determine si se trata de
+un numero par o impar.
+***********************************************************************/
+
+//Librerias
+#include <iostream>
+using namespace std;
+
+//Funcion principal.
+int main(){
+    //Declaracion de variables.
+        int num;
+    //Solicitar datos al usuario.
+        cout<<"Digite un numero: "; cin>>num;
+    //Acciones del programa (Mostrar resultados).
+        if(num==0){
+            cout<<"El numero es un valor nulo"<<endl;
+        }else if(num%2==0){
+            cout<<"El numero es un valor par"<<endl;
+        }else{
+            cout<<"El numero es un valor impar"<<endl;
+        }
+    return 0;
+}
