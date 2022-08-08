5a9e02617798f032e29186a23a91b07c396f5b1d
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-08 08:50:47 -0500

    Ejercicio #2 Mostrar la diagonal principal de una matriz

---
+++ Matriz/ej2_matriz.cpp
@@ -0,0 +1,34 @@
+/****************************************************************
+Realiza un programa que defina una matriz de 3x3 y escriba un
+ciclo para que muestre la diagonal principal de la matriz.
+****************************************************************/
+
+//Librerias.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//Funcion principal.
+int main(){
+    //Declaracion de variables.
+        int num[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    //Acciones del programa.
+        //Mostrar matriz inicial.
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                cout<<num[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    //Mostrar resultados del programa.
+        cout<<"\n";
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(i==j){
+                    cout<<num[i][j]<<endl;
+                }
+            }
+        }
+    getch();
+    return 0;
+}
