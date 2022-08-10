a9792bf6ee1600cb2b5244bf0871185c8c2010f4
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-10 12:31:56 -0500

    Funcion strrev (Invertir cadenas)

---
+++ Cadenas/fun_strrev.cpp
@@ -0,0 +1,12 @@
+// Funcion strreb() -> Invertir una cadena de caracteres.
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    char cad[] = "Bryan";
+    strrev(cad);
+    cout << cad << endl;
+    return 0;
+}
