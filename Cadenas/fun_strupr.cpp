585f95610a23670320e5427af9da36695ded0f06
Bryan Hernández López <bryan.hdezl2407@hotmail.com>
2022-08-10 13:27:52 -0500

    Funcion strupr (Convertir cadenas a mayusculas)

---
+++ Cadenas/fun_strupr.cpp
@@ -0,0 +1,12 @@
+// Funcion strupr() -> Transforma una cadena a mayusculas.
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    char cad[] = "Bryan";
+    strupr(cad);
+    cout << cad << endl;
+    return 0;
+}
