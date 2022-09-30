67aad535509151fb08693cd568102b3ad7d53acd
Bryan-Hdez <bryan.hdezl2407@gmail.com>
2022-09-30 10:09:29 -0500

    Funcion para mostrar elementos de una lista simple

---
+++ Listas/mostrarLaLista.cpp
@@ -0,0 +1,66 @@
+//Mostrar elementos de una lista.
+
+/***********************************************************************
+Para mostrar elementos de una lista, solo hay que seguir 3 pasos:
+    1. Crear un nuevo nodo (actual).
+    2. Igualar el puntero actual a lista.
+    3. Recorrer la lista de inicio a fin.
+***********************************************************************/
+
+//Librerias
+#include <bits/stdc++.h>
+using namespace std;
+
+//Estructura del nodo.
+struct Nodo{
+    int dato;
+    Nodo *siguiente;
+};
+
+//Prototipos de las funciones.
+void insertarNodo(Nodo *&, int);
+void mostrarLista(Nodo *);
+
+//Funcion principal.
+int main(){
+    //Variables de la funcion.
+    Nodo *lista = NULL;
+    //Acciones del programa.
+    insertarNodo(lista, 2);
+    insertarNodo(lista, 4);
+    insertarNodo(lista, 5);
+    insertarNodo(lista, 9);
+    insertarNodo(lista, 1);
+    mostrarLista(lista);
+    return 0;
+}
+
+//Funcion para mostrar el contenido de una lista.
+void mostrarLista(Nodo *lista){
+    Nodo *actual = new Nodo();
+    actual = lista;
+    cout << "\t Lista: ";
+    while (actual != NULL){
+        cout << actual->dato << " -> ";
+        actual = actual->siguiente;
+    }
+}
+
+//Funcion para insertar elementos en la lista.
+void insertarNodo(Nodo *&lista, int n){
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->dato = n;
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+    while ((aux1 != NULL) && (aux1->dato < n)) {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    if (lista == aux1){ //No entro al while anterior.
+        lista = nuevoNodo;  //Agregamos el primer nodo a la lista.
+    }else{
+        aux2->siguiente = nuevoNodo; //Agregamos otro nodo a la lista.
+    }
+    nuevoNodo->siguiente = aux1;  // EL puntero siguiente apuntara al elemento agregado.
+    cout << "Elemento " << n << " agregado a la lista." << endl;
+}
