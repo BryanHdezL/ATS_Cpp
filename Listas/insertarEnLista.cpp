c832503ef1d1c4468433a47a204348efe4c21650
Bryan-Hdez <bryan.hdezl2407@gmail.com>
2022-09-30 10:08:27 -0500

    Funcion para insertar elementos a una lista simple

---
+++ Listas/insertarEnLista.cpp
@@ -0,0 +1,54 @@
+//Insertar elementos en una lista.
+
+/***********************************************************************
+Para insertar elementos en una lista, solo hay que seguir 4 pasos:
+    1. Crear un nuevo nodo.
+    2. Asignar el nuevo nodo en dato al valor que se quiere agregar.
+    3. Crear dos nodos auxiliares y asignar la lista al primer nodo.
+    4. Insertar el elemento a la lista.
+***********************************************************************/
+
+//Librerias
+#include <bits/stdc++.h>
+using namespace std;
+
+//Estructura del nodo.
+struct Nodo{
+    int dato;
+    Nodo *siguiente;
+};
+
+//Prototipos de las funciones.
+void insertarNodo(Nodo *&, int);
+
+//Funcion principal.
+int main(){
+    //Variables de la funcion.
+    Nodo *lista = NULL;
+    //Acciones del programa.
+    insertarNodo(lista, 2);
+    insertarNodo(lista, 4);
+    insertarNodo(lista, 5);
+    insertarNodo(lista, 9);
+    insertarNodo(lista, 1);
+    return 0;
+}
+
+//Funcion para insertar elementos en la lista.
+void insertarNodo(Nodo *&lista, int n){
+    Nodo *nuevoNodo = new Nodo();
+    nuevoNodo->dato = n;
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+    while ((aux1 != NULL) && (aux1->dato < n)) {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    if (lista == aux1){ //No entro al while anterior.
+        lista = nuevoNodo;  //Agregamos el primer nodo a la lista.
+    }else{
+        aux2->siguiente = nuevoNodo; //Agregamos otro nodo a la lista.
+    }
+    nuevoNodo->siguiente = aux1;  // EL puntero siguiente apuntara al elemento agregado.
+    cout << "Elemento " << n << " agregado a la lista." << endl;
+}
